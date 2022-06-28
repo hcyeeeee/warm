cfecd921791bc6f3040f45c3cefd489aee294b9d
Allen <m1991allen@gmail.com>
2022-06-28 11:04:36 +0800

    fixing

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -10,13 +10,13 @@
                 <h5 class="mo">每日一篇暖新聞&nbsp;傳遞療癒正能量</h5>
                 <h5 class="momo">每日一篇暖新聞&nbsp;傳遞療癒正能量</h5>
                 <hr />
-                <p class="hi col-10 col-lg-11">
+                <p class="hi col-10 col-xl-11">
                     近日身處在疫情延燒、俄烏戰爭未見停歇、通膨風暴席捲全球的生活中，是否令你感到焦慮不安？或是高壓忙碌的工作，讓你感覺身心俱疲？<br /><br />
                     雖然日常生活裡，我們偶爾會低潮、憂鬱，但別忘了這個世界還是有很多好事發生！民視新聞網替大家精選療癒、正向暖新聞，希望為世界帶來一點正能量，鼓舞每個人帶著溫暖的心，繼續向前行。
                 </p>
             </div>
 
-            <div class="subtitle1 col-lg-5 ml-5">
+            <div class="subtitle1 col-xl-5 ml-5">
                 <h2>熱門精選Top4</h2>
                 <b-carousel
                     id="carousel-1"
